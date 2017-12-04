10b30a13ba3eebb906948d182ca5a1ed86336c94
eglaf <eglafx@gmail.com>
2017-12-04 15:23:34 +0100

    very basics

---
+++ src/Controller/Admin/DashboardController.php
@@ -0,0 +1,30 @@
+<?php
+
+namespace App\Controller\Admin;
+
+use Symfony\Bundle\FrameworkBundle\Controller\Controller;
+use Symfony\Component\Routing\Annotation\Route;
+use Sensio\Bundle\FrameworkExtraBundle\Configuration\Template;
+
+/**
+ * Class DashboardController
+ */
+class DashboardController extends Controller {
+	
+	/**
+	 * Dashboard of admin.
+	 *
+	 * RouteName: app_admin_dashboard_index
+	 * @Route("/admin")
+	 * @Template
+	 * @return array
+	 */
+	public function indexAction() {
+		$number = mt_rand(0, 100);
+		
+		return [
+			'number' => $number,
+		];
+	}
+
+}
